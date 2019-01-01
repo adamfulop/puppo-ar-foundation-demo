cecc0d981ba350feda9fc46dead12584c2d844bb
Adam Fulop <adam.fulop93@gmail.com>
2019-01-01 20:22:44 +0100

    init commit

---
+++ Assets/PuppoTheCorgi/Fetch/Scripts/ARScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARScene : MonoBehaviour {
+    [SerializeField] private ARPlaneManager arPlaneManager;
+
+    private void Start() {
+        arPlaneManager.planeUpdated += PlaneUpdated;
+    }
+
+    private void PlaneUpdated(ARPlaneUpdatedEventArgs args) {
+        var planeTransform = args.plane.transform;
+        var objectTransform = transform;
+        objectTransform.position = planeTransform.position;
+        objectTransform.rotation = planeTransform.rotation;
+    }
+}
